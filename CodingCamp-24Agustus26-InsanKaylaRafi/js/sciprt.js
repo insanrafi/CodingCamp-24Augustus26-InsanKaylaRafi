827d6b728a24fd07054f89a2e648a9d166ef78ac
insanrafi <insankaylarafi01@gmail.com>
2026-08-29 10:54:48 +0700

    -

--- CodingCamp-24Agustus26-InsanKaylaRafi/js/sciprt.js
+++ CodingCamp-24Agustus26-InsanKaylaRafi/js/sciprt.js
@@ -325,3 +325,28 @@ if (savedQuickLinks) {
     quickLinks = JSON.parse(savedQuickLinks);
     renderQuickLinks();
 }
+
+
+
+const themeToggle = document.getElementById("themeToggle");
+
+themeToggle.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeToggle.textContent = "Light Mode";
+        localStorage.setItem("theme", "dark");
+    } else {
+        themeToggle.textContent = "Dark Mode";
+        localStorage.setItem("theme", "light");
+    }
+});
+
+
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    themeToggle.textContent = "Light Mode";
+}
